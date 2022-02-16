4b33df571e849df451d5112109a8ace050c530b9
basdawimp <basharoldknight@gmail.com>
2022-02-16 19:39:03 +0000

    added method to update an existing product.
    Updates an existing product with new inputs given via a patch request.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -22,6 +22,21 @@ export class ProductsService {
     return { ...product };
   }
 
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
   private findProduct(id: string): [Product, number] {
     const productIndex = this.products.findIndex((prod) => prod.id === id);
     const product = this.products[productIndex];
